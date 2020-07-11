401d4aee76cd6e6b965bf49e1a42beba3e2e2663
Eddie Brazier <edwinbrazier01@gmail.com>
2020-07-11 16:08:07 -0400

    added elements for advancing levels and respawning the player

---
+++ MintTopaz-GMTKJam2020/Assets/Scripts/AdvanceLevel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//enum for the game's scenes
+public enum GameLevel
+{ 
+    Menu,
+    One,
+    Two
+}
+
+
+public class AdvanceLevel : MonoBehaviour
+{
+    //game level enum for determing where the goal sends the player
+    public GameLevel nextLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //check if the player is colliding with the level exit
+        if (other.gameObject.GetComponent<PlayerController>() != null)
+        {
+            //load the next level
+            SceneManager.LoadScene((int)nextLevel);
+        }
+    }
+}
---
+++ MintTopaz-GMTKJam2020/Assets/Scripts/KillPlane.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlane : MonoBehaviour
+{
+    //transform to spawn the player back at
+    public Transform spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //check if the player is colliding with the kill plane
+        if(collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            //move the player to the spawnpoint
+            collision.gameObject.transform.position = spawnPoint.position;
+        }
+    }
+}
